eaa193e3a3d15e10b7428a316d4051f73079453b
tomRob123 <59135978+tomRob123@users.noreply.github.com>
2021-06-20 11:23:37 +0300

    small changes in directories

---
+++ src/pages/favorites/FavoritePlayersPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-jumbotron
+      header="Watch Your Favorite Players Here"
+    >
+    </b-jumbotron>
+    <favorite-players
+      :players=""
+    >
+    </favorite-players>
+  </div>
+</template>
+
+<script>
+import FavoritePlayers from "../../components/player/PlayerCollection.vue";
+export default {
+  components: {
+    "favorite-players": FavoritePlayers,
+  },
+  data() {
+    return {
+      allFavoriteMatches: [],
+    };
+  },
+  methods: {
+    async getFavoriteMatches() {
+      this.allFavoriteMatches = await this.updateMatches();
+    },
+  },
+  async created() {
+    await this.getFavoriteMatches();
+  },
+};
+</script>
+
+<style></style>
