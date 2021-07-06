57edd62a5dea3bb08e176668ecf9260b2e1706c3
tomRob123 <59135978+tomRob123@users.noreply.github.com>
2021-07-06 15:57:16 +0300

    small refactor

--- src/pages/search/SearchPage.vue
+++ src/pages/search/SearchPage.vue
@@ -133,51 +133,6 @@ export default {
           throw '';
         }
 
-        // let search_results = [
-        //   {
-        //     id: 23664,
-        //     full_name: "Hans Jorge Mulder",
-        //     common_name: "H. Mulder",
-        //     country: "Netherlands",
-        //     birth_date: "27/04/1987",
-        //     height: "180 cm",
-        //     weight: "76 kg",
-        //     image:
-        //       "https://cdn.sportmonks.com/images/soccer/players/16/23664.png",
-        //     player_position: 3,
-        //     team_name: "RKC Waalwijk",
-        //     in_favorites: false,
-        //   },
-        //   {
-        //     id: 82959,
-        //     full_name: "Hans Henrik Andreasen",
-        //     common_name: "H. Andreasen",
-        //     country: "Denmark",
-        //     birth_date: "10/01/1979",
-        //     height: "183 cm",
-        //     weight: "72 kg",
-        //     image:
-        //       "https://cdn.sportmonks.com/images/soccer/players/15/82959.png",
-        //     player_position: 3,
-        //     team_name: "OB",
-        //     in_favorites: false,
-        //   },
-        //   {
-        //     id: 24081697,
-        //     full_name: "Hans Christian Bernat",
-        //     common_name: "H. Bernat",
-        //     country: "Denmark",
-        //     birth_date: "13/11/2000",
-        //     height: null,
-        //     weight: null,
-        //     image:
-        //       "https://cdn.sportmonks.com/images/soccer/players/1/24081697.png",
-        //     player_position: 1,
-        //     team_name: "OB",
-        //     in_favorites: true,
-        //   },
-        // ];
-
         if (this.searchCategory == "player" && this.filterBy != null) {
           search_results = this.filterPlayers(search_results);
         }
